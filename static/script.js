1722e7f6dc5384e14f8aeace879c1896212cc4ea
Florian Pigorsch <fpigorsch@gmail.com>
2025-08-13 12:32:00 +0200

    add umami switch

--- static/script.js
+++ static/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // MAPS
     var mapDiv = document.getElementById('club-map');
     if (mapDiv) {
         var lat = parseFloat(mapDiv.dataset.lat);
@@ -11,5 +12,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }).addTo(map);
         L.marker([lat, lon]).addTo(map)
             .bindPopup(name).openPopup();
+    }
+
+    // UMAMI
+    document.querySelectorAll("a[target=_blank]").forEach((a) => {
+        if (a.getAttribute("data-umami-event") === null) {
+            a.setAttribute('data-umami-event', 'outbound-link-click');
+        }
+        a.setAttribute('data-umami-event-url', a.href);
+    });
+    if (location.hash === '#disable-umami') {
+        localStorage.setItem('umami.disabled', 'true');
+        alert('Umami is now DISABLED in this browser.');
+    }
+    if (location.hash === '#enable-umami') {
+        localStorage.removeItem('umami.disabled');
+        alert('Umami is now ENABLED in this browser.');
     }
 });
